796bbf956b68f1db974c97094769cf7eb3aaec17
ignismeow <saadbeenco@gmail.com>
2024-08-19 14:43:50 +0500

    UpdateArray.js added new file wwhich

--- seven/src/App.jsx
+++ seven/src/App.jsx
@@ -1,16 +1,18 @@
 // import Button from "./Button.jsx";
 // import Counter from "./Counter.jsx";
 // import MyComponentss from "./UpdaterFuntion.jsx";
-import UpdatingState from "./UpdatingState.jsx";
+// import UpdatingState from "./UpdatingState.jsx";
 // import ColorPicker from "./ColorPicker.jsx";
 // import MyComponents from "./MyComponents.jsx";
 // import MyComponent from "./MyComponent.jsx";
 // import ProfilePicture from "./profilepic.jsx";
 
+import UpdateArray from "./UpdateArray";
+
 function App() {
   
   return(<>
-    <UpdatingState/>
+    <UpdateArray/>
   </>);
 
 }
